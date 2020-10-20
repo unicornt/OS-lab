c92bda4308790622aa8c51d464a84850cfa4d4b9
unicornt <unicorntwin@outlook.com>
2020-10-20 13:44:36 +0800

    finish testing vm_free. coding finish

--- kern/main.c
+++ kern/main.c
@@ -27,14 +27,26 @@ main()
     void *pgdir = kalloc();
     memset(pgdir, 0, PGSIZE);
     map_region(pgdir, (void*)0, (uint64_t)PGSIZE, V2P(p), 0);
+    cprintf("pgdir %x  p %x\n", pgdir, p);
     memset(p, 0xFF, PGSIZE);
     asm volatile("msr ttbr0_el1, %[x]": : [x]"r"(V2P(pgdir))); 
-    cprintf("start test\n");
+    cprintf("start testing map_region\n");
     for(uint64_t i = 0;i  < PGSIZE; i++) {
         if((*((char*)i)) != 0xFF){
             cprintf("error in %d : %x\n", i, *((char*)i));
         }
     }
-    cprintf("testing finish\n");
+    asm volatile("mrs x9, ttbr1_el1"); 
+    asm volatile("msr ttbr0_el1, x9"); 
+    cprintf("finish testing map_region\n");
+    cprintf("start testing vm_free\n");
+    vm_free(pgdir, 0);
+    cprintf("p %x\n", p);
+    for(int i = 8;i < PGSIZE; i++) { // first 8 bytes are next free list information
+        if((*((char*)((uint64_t)p + i))) != 0x1) {
+            cprintf("error in %d : %x\n", i, *(char*)((uint64_t)p +i ));
+        }
+    }
+    cprintf("finish testing vm_free\n");
     while (1) ;
 }
