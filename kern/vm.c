2bddaf195a86c635fe56e5a0895ce3428b9f2491
unicornt <unicornt@unicornt-MacBook-Pro.local>
2020-10-23 16:23:48 +0800

    fix pte locate

--- kern/vm.c
+++ kern/vm.c
@@ -29,7 +29,7 @@ pgdir_walk(uint64_t *pgdir, const void *va, int64_t alloc)
     if((uint64_t)va >= (1L << 50)) panic("virtual memory limit exceed");
     uint64_t *pg = pgdir;
     for(int level = 0; level < 3; level++) {
-        uint64_t *pte = (uint64_t)pg | (PTX(level, va) << 3); // pte point to target entry in this page table
+        uint64_t *pte = (uint64_t)pg | (PTX(level, va)); // pte point to target entry in this page table
         if(*pte & PTE_P) { // check valid
             pg = (uint64_t*) P2V(PTE_ADDR(*pte));
         }
@@ -39,7 +39,7 @@ pgdir_walk(uint64_t *pgdir, const void *va, int64_t alloc)
             *pte = V2P(pg) | PTE_TABLE | PTE_P;
         }
     }
-    return (uint64_t)pg | (PTX(3, va) << 3);
+    return (uint64_t)pg | (PTX(3, va));
     /* My code ends. */
 }
 
@@ -80,7 +80,7 @@ vm_free(uint64_t *pgdir, int level)
     /* TODO: Your code here. */
     if(pgdir == 0) panic("pgdir not found");
     if(level < 3) {
-        for(int i = 0; i < PGSIZE; i += 8) {
+        for(int i = 0; i < PGSIZE; i ++) {
             uint64_t *pte = (uint64_t)pgdir | i;
             if(*pte & PTE_P) {
                 vm_free(P2V(PTE_ADDR(*pte)), level + 1);
@@ -88,7 +88,7 @@ vm_free(uint64_t *pgdir, int level)
         }
     }
     else {
-        for(int i = 0; i < PGSIZE; i += 8) {
+        for(int i = 0; i < PGSIZE; i ++) {
             uint64_t* pte = (uint64_t)pgdir | i;
             if(*pte & PTE_P) {
                 cprintf("%x\n", P2V(PTE_ADDR(*pte)));
