d8004ec76a300ddd012c98c1b1a926dedd71cc37
unicornt <unicorntwin@outlook.com>
2020-10-25 10:35:09 +0800

    delete backup callee-save register

--- inc/trap.h
+++ inc/trap.h
@@ -8,6 +8,7 @@ struct trapframe {
     uint64_t SPSR_EL1; // PSTATE
     uint64_t ELR_EL1; // pc
     uint64_t SP_EL0; // sp in user mode
+    uint64_t x0;
     uint64_t x1;
     uint64_t x2;
     uint64_t x3;
@@ -26,17 +27,6 @@ struct trapframe {
     uint64_t x16;
     uint64_t x17;
     uint64_t x18;
-    uint64_t x19;
-    uint64_t x20;
-    uint64_t x21;
-    uint64_t x22;
-    uint64_t x23;
-    uint64_t x24;
-    uint64_t x25;
-    uint64_t x26;
-    uint64_t x27;
-    uint64_t x28;
-    uint64_t x29;
     uint64_t x30;
 };
 
